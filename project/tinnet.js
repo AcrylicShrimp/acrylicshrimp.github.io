3be5354f58dce04126a21bd8311478c4fb1fc6e8
AcrylicShrimp <led789zxpp@naver.com>
2019-09-15 23:57:13 +0900

    Added a graph in TinNet project page.

--- project/tinnet.js
+++ project/tinnet.js
@@ -1,3 +1,41 @@
 
 'use strict';
 
+function onReady(callback) {
+    // in case the document is already rendered
+    if (document.readyState != 'loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function () {
+        if (document.readyState == 'complete') callback();
+    });
+}
+
+onReady(function () {
+    new Chart('graph', {
+        type: 'bar',
+        data: {
+            labels: ['Naive', 'SSE', 'AVX', 'AVX + OpenMP'],
+            datasets: [{
+                label: 'sec/epoch',
+                data: [270, 86, 44, 13],
+                backgroundColor: [
+                    '#FC5656',
+                    '#56FCB4',
+                    '#29E2FF',
+                    '#E974FF'
+                ]
+            }]
+        },
+        options: {
+            scales: {
+                yAxis: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+});
